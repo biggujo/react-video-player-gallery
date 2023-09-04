6517aad8dca552a3cba238da869b0fe8d7a37c7b
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-09-04 12:48:20 +0300

    Add Player component with react-player

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactPlayer from 'react-player/vimeo';
+import PropTypes from 'prop-types';
 
-export default function Player({ url }) {
-  return (<ReactPlayer url={url}
-                       controls={true} />);
+class Player extends Component {
+  static propTypes = {
+    url: PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVideoLoaded: false,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log('Update!');
+
+    if (prevProps.url !== this.props.url) {
+      this.setState({ isVideoLoaded: false });
+    }
+  }
+
+  handleReady = () => {
+    this.setState({ isVideoLoaded: true });
+  };
+
+  render() {
+    const { url } = this.props;
+    const { isVideoLoaded } = this.state;
+
+    const shouldShowLoader = url && !isVideoLoaded;
+    const playerSize = isVideoLoaded ? '600px' : 0;
+
+    return (<>
+      {shouldShowLoader && <h2>Please, stand by...</h2>}
+      {url && <ReactPlayer url={url}
+                           controls={true}
+                           width={playerSize}
+                           onReady={this.handleReady} />}
+    </>);
+  }
 }
+
+export default Player;
